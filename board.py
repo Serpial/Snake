3d005b32e7150fe820f919fc30ed9f9abe0c0d81
serpial <phutchison8@gmail.com>
2019-09-05 23:19:17 +0100

    The snake now moves and can be controlled by the user

--- board.py
+++ board.py
@@ -10,33 +10,73 @@ class Board (turtle.Turtle):
         self.width = size
         self.between_squares = 20 # Disatance between center of squares
         self.board = self.new_board()
-
-        self.snake_length = 1
+        self.snake_controls = ["Up", "Right", "Down", "Left"]
         self.snake_directions = ["north", "east", "south", "west"]
-        self.current_direction = self.snake_directions[randint(0,len(self.snake_directions)-1)]
-
+        self.current_direction = self.snake_directions[\
+              randint(0,len(self.snake_directions)-1)\
+        ]
+        
+        # Set the middle as the starting position for the snake
         self.snake_pieces = [(floor(self.width/2), floor(self.width/2))]
 
     # create game board given that the center of the screen is (0, 0)
     def new_board(self):
-        board = [[boardPiece.BoardPiece() for y in range(self.width)] for x in range(self.width)]
-        y = -self.between_squares*(self.width/2)
-        x = -self.between_squares*(self.width/2)
+        board = [[boardPiece.BoardPiece() for y in range(self.width)]\
+                 for x in range(self.width)]
+        y = (-self.between_squares)*(self.width/2)
+        x = (-self.between_squares)*(self.width/2)
         for i in range(self.width):
             for j in range(self.width):
                 board[i][j].setposition(x, y)
-                x += self.between_squares
-            y += self.between_squares
-            x = -self.between_squares*(self.width/2)
-
+                y += self.between_squares
+            x += self.between_squares
+            y = -(self.between_squares)*(self.width/2)
         return board
 
     def activate_snake_pieces(self):
+        # First Deactivate all snake pieces
+        for i in range(self.width):
+            for j in range(self.width):
+                self.board[i][j].remove_snake_piece()
+        # Then reactivate the right ones
         for i in range(len(self.snake_pieces)):
-            self.board[self.snake_pieces[i][0]][self.snake_pieces[i][1]].make_snake_piece()
+            self.board[self.snake_pieces[i][0]]\
+                [self.snake_pieces[i][1]].make_snake_piece()
         
             
     def move_in_direction(self):
-        if self.current_direction = "north":
-            
+        next_square = (0,0)
+        head_of_snake = self.snake_pieces[0]
+        if self.current_direction == "north":
+            next_square = (head_of_snake[0], head_of_snake[1]+1)
+        if self.current_direction == "east":
+            next_square = (head_of_snake[0]+1, head_of_snake[1])
+        if self.current_direction == "south":
+            next_square = (head_of_snake[0], head_of_snake[1]-1)
+        if self.current_direction == "west":
+            next_square = (head_of_snake[0]-1, head_of_snake[1])
+
+        print("Head: {}, Next: {}".format(head_of_snake, next_square))
+        # If the snake has ate an apple it can grow, otherwise
+        #    the last item is lost in the movement
+        if (next_square[0] < 0 or next_square[1] < 0) or\
+             (next_square[0] >= self.width or next_square[1] >= self.width):
+            return False
+        elif self.board[next_square[0]][next_square[1]].is_apple:
+            self.snake_pieces.insert(0, next_square)
+        else:
+            self.snake_pieces.insert(0, next_square)
+            self.snake_pieces.pop()
+        return True
     
+    def set_control_up(self):
+        self.current_direction = self.snake_directions[0]
+
+    def set_control_right(self):
+        self.current_direction = self.snake_directions[1]
+
+    def set_control_down(self):
+        self.current_direction = self.snake_directions[2]
+        
+    def set_control_left(self):
+        self.current_direction = self.snake_directions[3]
--- snake.py
+++ snake.py
@@ -6,29 +6,40 @@ from math import floor
 # Set up window
 window = turtle.Screen()
 window.title("Snake")
-window.setup(width=600, height=600)
+window.setup(width=420, height=420)
 window.bgcolor("white")
 window.tracer(0)
 
-move_speed = 1.5  # In Seconds
+move_speed = 0.75  # In Seconds
 
 board = board.Board(19)
 board.activate_snake_pieces()
 
+# Set up controls
+window.onkeypress(board.set_control_up, board.snake_controls[0])
+window.onkeypress(board.set_control_right, board.snake_controls[1])
+window.onkeypress(board.set_control_down, board.snake_controls[2])
+window.onkeypress(board.set_control_left, board.snake_controls[3])
+window.listen()
+
 # Start Movements
 continue_playing = True
-
 previous_time = floor(time.time())
+previous_direction = board.current_direction
+print("Starting direction : " + board.current_direction)
 while continue_playing:
+
     window.update()
     current_time = floor(time.time())
-    if current_time > (previous_time + move_speed):
+    # Set up an interupt here for user input
+    if board.current_direction != previous_direction or\
+    current_time > (previous_time + move_speed):
         previous_time = current_time
-
-        # move snake
-        print("here")
+        previous_direction = board.current_direction
+        previous_time = current_time
+        continue_playing = board.move_in_direction()
         board.activate_snake_pieces()
         
 print("Game Over")
 turtle.mainloop()
-    
+
